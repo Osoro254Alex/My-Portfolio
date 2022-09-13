2f3d2590be7b22e1986760d191957b795f335891
Alex Ogeto <103739438+Osoro254Alex@users.noreply.github.com>
2022-09-13 11:28:12 +0300

    Add App.js  files

---
+++ app.js
@@ -0,0 +1,17 @@
+const navSectionOne = document.querySelector('.info');
+const navSectionTwo = document.querySelector('.menu-icon');
+const links = document.querySelectorAll('.co');
+const sections = document.querySelectorAll('.sections');
+
+function menuTransition() {
+  if (navSectionOne.classList.contains('phone')) {
+    navSectionOne.classList.remove('phone');
+  } else {
+    navSectionOne.classList.add('phone');
+    sections.forEach((sect) => {
+      sect.classList.toggle('active');
+    });
+  }
+}
+
+navSectionTwo.addEventListener('click', menuTransition);
