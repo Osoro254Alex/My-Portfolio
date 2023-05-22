6f422c4e3499bc493376127f7d80365a0c0a5235
Alex Ogeto <alexogeto54@gmail.com>
2023-05-22 19:53:15 +0300

    Update HTML, CSS and JavaScript files

--- app.js
+++ app.js
@@ -353,6 +353,7 @@ butt.addEventListener('click', (e) => {
       .map((tech) => `<li>${tech}</li>`)
       .join('');
     if (klas === object.id) {
+      
       windowPopup.innerHTML = `<div id="${object.id}" class="pop-up">
        <div class="pop-head">
          <h2 class="ton-del">
